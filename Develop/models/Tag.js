0660fa92ea4827d787fbfa1234934670e1cac5d3
jwm <jwm3926@gmail.com>
2022-11-26 17:05:58 -0500

    created all of the models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true
+      }
+    },
+    tag_name:{
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
